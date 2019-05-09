46af8d4fae13fd8da756c8e67a1ead5d11b0e3be
TW1943 <tw1943@gmail.com>
2019-05-10 06:19:33 +0800

    fix sqli login.php

--- sqli/login.php
+++ sqli/login.php
@@ -1,37 +1,38 @@
 <?php
-	$db_host="localhost";
-	$db_user="newuser";
-	$db_pass="123";
-	$db_name="my_db";
-	$flag = "LSA {sqli is great?!}";
+$db_host = "ctf.bluet.org";
+$db_user = "newuser";
+$db_pass = "123";
+$db_name = "my_db";
+$db_port = "33061";
+$flag = "LSA {sqli is great?!}";
 
+$user = $_POST['username'];
+$pass = $_POST['password'];
 
-		if(!empty($_POST['username']) && !empty($_POST['password'])) {
-		    $connection = sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', $db_host, $db_name);
-		    $db = new PDO($connection, $db_user, $db_pass);
-		    $sql = sprintf("SELECT * FROM `account` WHERE `name` = '%s' AND `passwd` = '%s'",
-		        $_POST['username'],
-		        $_POST['password']
-		    );
-		    try {
-		        $query = $db->query($sql);
-		        if($query) {
-		            $user = $query->fetchObject();
-		        } else {
-		            $user = false;
-		        }
-		    } catch(Exception $e) {
-		        $user = false;
-		    }
-		}
-?>
-<?php if(!$user): ?>
-<?php if($user === false): ?>
-<?=$sql?>
-<div class="alert alert-danger">login fail</div>
-<?php endif; ?>
-<?php else: ?>
-<h3>Hi, <?=htmlentities($user->name)?></h3>
-<h4><?=sprintf("You %s admin!", $user->name === "admin" ? "are" : "are not")?></h4>
-<?php if($user->name === "admin") printf("%s %s", htmlentities("hello admin this is your flag :"),$flag); ?>
-<?php endif; ?>
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+
+    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+    //$query = "SELECT uname,upass FROM wargame1 WHERE uname = '$user' AND upass = '$pass'";
+    $sql = "SELECT * FROM account WHERE name = '$user' AND passwd = '$pass'";
+
+    if (!$result = $mysqli->query($sql)) {
+        echo "Sorry, the website is experiencing problems.";
+        echo "Error: Our query failed to execute and here is why: \n";
+        echo "Query: " . $sql . "\n";
+        echo "Errno: " . $mysqli->errno . "\n";
+        echo "Error: " . $mysqli->error . "\n";
+        exit;
+    }
+    if ($result->num_rows === 0) {
+        echo "rows = 0?";
+        exit;
+    }
+
+    $dbdata = $result->fetch_assoc();
+
+    echo htmlentities($dbdata["name"]);
+    echo sprintf("You %s admin!", $dbdata["name"] === "admin" ? "are" : "are not");
+
+    if ($dbdata["name"] === "admin") {
+        printf("%s %s", htmlentities("hello admin this is your flag :"), $flag);
+    }
