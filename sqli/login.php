582db10e0ef3fd1278b217c663d0b31b67247d60
TW1943 <tw1943@gmail.com>
2019-05-10 06:22:39 +0800

    beautiful sqli/login.php

--- sqli/login.php
+++ sqli/login.php
@@ -12,12 +12,9 @@ $pass = $_POST['password'];
 if (!empty($_POST['username']) && !empty($_POST['password'])) {
 
     $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
-    //$query = "SELECT uname,upass FROM wargame1 WHERE uname = '$user' AND upass = '$pass'";
     $sql = "SELECT * FROM account WHERE name = '$user' AND passwd = '$pass'";
 
     if (!$result = $mysqli->query($sql)) {
-        echo "Sorry, the website is experiencing problems.";
-        echo "Error: Our query failed to execute and here is why: \n";
         echo "Query: " . $sql . "\n";
         echo "Errno: " . $mysqli->errno . "\n";
         echo "Error: " . $mysqli->error . "\n";
@@ -30,8 +27,8 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
 
     $dbdata = $result->fetch_assoc();
 
-    echo htmlentities($dbdata["name"]);
-    echo sprintf("You %s admin!", $dbdata["name"] === "admin" ? "are" : "are not");
+    echo "Hi!".htmlentities($dbdata["name"])."<\br>";
+    echo sprintf("You %s admin!", $dbdata["name"] === "admin" ? "are" : "are not")."<\br>";
 
     if ($dbdata["name"] === "admin") {
         printf("%s %s", htmlentities("hello admin this is your flag :"), $flag);
