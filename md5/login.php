c932f7b9ed0f6749371254fd557a8b9db2b677ce
CTF <tw1943@gmail.com>
2019-05-11 21:51:53 +0000

    fix php&source;

--- md5/login.php
+++ md5/login.php
@@ -1,7 +1,8 @@
 <?php
 	$username = $_POST['username'];
 	$passwd = $_POST['password'];
-	if($username=="admin" && md5($passwd)=="0e509367213418206700842008763514"){
+	//set a md5 not in the world;
+	if($username=="admin" && md5($passwd)=="0e394748910394873823912398437829"){
 		echo "y0u Ar3 a HaCK3R".'<br>'.'<br>';
 		system("cat flag_md5.txt");
 	}else{
