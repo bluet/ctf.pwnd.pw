7c0f01b7309bd538c5db713ae27859a975b0a659
CTF <tw1943@gmail.com>
2019-05-11 21:58:56 +0000

    fix just req .txt to see key?

--- md5/login.php
+++ md5/login.php
@@ -1,10 +1,11 @@
 <?php
+	$key = "LSA-FLAG{ should use '===' in php }";
 	$username = $_POST['username'];
 	$passwd = $_POST['password'];
 	//set a md5 not in the world;
 	if($username=="admin" && md5($passwd)=="0e394748910394873823912398437829"){
 		echo "y0u Ar3 a HaCK3R".'<br>'.'<br>';
-		system("cat flag_md5.txt");
+		echo $key;
 	}else{
 		echo "username or password is wrong"."<br>";
 		echo "please come back";
